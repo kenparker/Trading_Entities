beb8a0193d3a062a5b7c40de40b8a9cb6964d279
maggioni <maggioni@W70822.zv.tum.de>
2014-05-21 09:33:49 +0200

    New Logging Version where the FileHandler is closed and the Logger is retrieved with getter

--- src/Logger/MyLogger.java
+++ src/Logger/MyLogger.java
@@ -7,33 +7,53 @@ import java.util.logging.Handler;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-public class MyLogger
-{
+public class MyLogger {
 
-    private static final String FILE_PATH = "C:/Users/magang/Documents/Logs/";
-    static private FileHandler fileHdlr;
-    static private MyFormatter formatterTxt;
-    static private Handler conHdlr;
-    static boolean firsttime = true;
+    //private static final String FILE_PATH = "C:/Users/magang/Documents/Logs/";
+    private static final String FILE_PATH = System.getProperty("user.dir") + "\\";
+    private static FileHandler fileHdlr;
+    private static MyFormatter formatterTxt;
+    private static Handler conHdlr;
+    private static boolean firsttime = true;
 
     private static final Logger logger = Logger.getLogger("StockServiceLogger");
 
-    static public void setup() throws IOException
-    {
+    static public void setup()  {
         if (firsttime) {
-            logger.setUseParentHandlers(false);
-            fileHdlr = new FileHandler(FILE_PATH + "StockServiceLogging.txt", true);
-            conHdlr = new ConsoleHandler();
-            formatterTxt = new MyFormatter();
-            fileHdlr.setFormatter(formatterTxt);
-            fileHdlr.setLevel(Level.ALL);
-            conHdlr.setFormatter(formatterTxt);
-            conHdlr.setLevel(Level.ALL);
-            logger.addHandler(fileHdlr);
-            logger.addHandler(conHdlr);
-
-            firsttime = false;
+            try {
+                System.out.println("Log File :"+FILE_PATH + "StockServiceLogging.txt");
+                logger.setUseParentHandlers(false);
+                fileHdlr = new FileHandler(FILE_PATH + "StockServiceLogging.txt", true);
+                conHdlr = new ConsoleHandler();
+                formatterTxt = new MyFormatter();
+                fileHdlr.setFormatter(formatterTxt);
+                fileHdlr.setLevel(Level.ALL);
+                conHdlr.setFormatter(formatterTxt);
+                conHdlr.setLevel(Level.ALL);
+                logger.addHandler(fileHdlr);
+                logger.addHandler(conHdlr);
+                logger.setLevel(Level.INFO);
+                
+                firsttime = false;
+                
+                logger.finer("Logger setup done");
+            } catch (IOException ex) {
+                Logger.getLogger(MyLogger.class.getName()).log(Level.SEVERE, null, ex);
+            } catch (SecurityException ex) {
+                Logger.getLogger(MyLogger.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        } else {
+            logger.finer("Logger is already setup");
         }
     }
 
+    static public void closeHandler() {
+        fileHdlr.close();
+    }
+
+    public static Logger getLogger() {
+        return logger;
+    }
+
+    
 }
